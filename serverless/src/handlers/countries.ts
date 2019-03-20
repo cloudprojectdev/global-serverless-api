6eec7469864988e635d27232bd753f83df433415
Cloud Project <cloudproject@Nehemiah-2.local>
2019-03-20 12:41:04 -0400

    Adding fetch all countries service

--- serverless/src/handlers/countries.ts
+++ serverless/src/handlers/countries.ts
@@ -28,3 +28,26 @@ export async function getCountry(event: any, context: Context) {
     result.Region = process.env.AWS_REGION;
     return new Result(Status.OK, result).asLambdaResult();
 }
+
+export async function getCountries(event: any, context: Context) {
+    var params:AWS.DynamoDB.DocumentClient.ScanInput = {
+        TableName: 'Countries',
+        AttributesToGet: [
+            'id',
+            'name'
+        ],
+        Limit: 200
+    }
+    let result
+    try {
+        result = await documentClient.scan(params).promise()
+    } catch(err) {
+        return new Result(Status.InternalServerError, { error: err.message }).asLambdaResult()
+    }
+    let output = {
+        Countries: result.Items,
+        Count: result.Items.length,
+        Region: process.env.AWS_REGION
+    }
+    return new Result(Status.OK, output).asLambdaResult()
+}
